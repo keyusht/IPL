24fb2c2a08ccad5e71f54771d81b6510b8e3f600
keyusht <135361060+keyusht@users.noreply.github.com>
2025-09-06 16:00:33 +0530

    Update TeamRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
@@ -1,12 +1,16 @@
 package com.wecp.progressive.repository;
 
+import com.wecp.progressive.entity.Team;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.wecp.progressive.entity.Team;
-
 @Repository
-public interface TeamRepository extends JpaRepository<Team, Integer>{
-    Team findByTeamId(int teamId);
+public interface TeamRepository extends JpaRepository<Team, Integer> {
 
+   Optional<Team> findByTeamId(int teamId);
+
+   Optional<Team> findByTeamName(String name);
 }
