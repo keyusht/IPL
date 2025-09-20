efb41e63abf71272ca26727c89c80a11019234fb
username <wecp-dummy@gmail.com>
2025-09-20 12:50:34 +0000

    Day24 done

--- backend/src/main/java/com/wecp/progressive/entity/TicketBooking.java
+++ backend/src/main/java/com/wecp/progressive/entity/TicketBooking.java
@@ -1,5 +1,70 @@
 package com.wecp.progressive.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class TicketBooking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bookingId;
+
+    @ManyToOne
+    @JoinColumn(name = "match_id", nullable = false)
+    private Match match;
+
+    private String email;
+
+    private int numberOfTickets;
+
+    public TicketBooking(Integer bookingId,  String email, Match match,int numberOfTickets) {
+        this.bookingId = bookingId;
+        this.match = match;
+        this.email = email;
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    public TicketBooking() {
+    }
+
+    public Integer getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(Integer bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Match getMatch() {
+        return match;
+    }
+
+    public void setMatch(Match match) {
+        this.match = match;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNumberOfTickets() {
+        return numberOfTickets;
+    }
+
+    public void setNumberOfTickets(int numberOfTickets) {
+        this.numberOfTickets = numberOfTickets;
+    }
+
+    
 
 }
--- frontend/src/app/ipl/components/dashboard/dashboard.component.ts
+++ frontend/src/app/ipl/components/dashboard/dashboard.component.ts
@@ -1,5 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { IplService } from '../../services/ipl.service';
+import { Team } from '../../types/Team';
+import { Cricketer } from '../../types/Cricketer';
+import { Match } from '../../types/Match';
+import { TicketBooking } from '../../types/TicketBooking';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Vote } from '../../types/Vote';
 
-export class DashboardComponent  {
- 
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  teams: Team[] = [];
+  cricketers: Cricketer[] = [];
+  matches: Match[] = [];
+  ticketsBooked: TicketBooking[] = [];
+  voteList: Vote[];
+  emailForm: FormGroup;
+  role: string| null;
+  userId: number; 
 
+  constructor(private iplService: IplService, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.role = localStorage.getItem("role");
+    this.userId = Number(localStorage.getItem("user_id"));
+    this.emailForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+    this.loadTeams();
+    this.loadCricketers();
+    this.loadMatches();
+    this.loadVotes();
+  }
+
+  loadTeams(): void {
+    this.iplService.getAllTeams().subscribe((teams) => {
+      this.teams = teams;
+    });
+  }
+
+  loadCricketers(): void {
+    this.iplService.getAllCricketers().subscribe((cricketers) => {
+      this.cricketers = cricketers;
+    });
+  }
+
+  loadMatches(): void {
+    this.iplService.getAllMatches().subscribe((matches) => {
+      this.matches = matches;
+    });
+  }
+
+  loadVotes(): void {
+    this.iplService.getAllVotes().subscribe((votes) => {
+      this.voteList = votes;
+    });
+  }
+
+  loadTicketsBooked(): void {
+    const email = this.emailForm.get('email')?.value;
+    this.iplService.getBookingsByUserEmail(email).subscribe((ticketsBooked) => {
+      this.ticketsBooked = ticketsBooked;
+    });
+  }
+
+  onSubmitEmail(): void {
+    if (this.emailForm.valid) {
+      this.loadTicketsBooked();
+    }
+  }
 }
